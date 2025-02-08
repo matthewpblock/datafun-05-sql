65f7c2634060146ce9be0d2b4a48eecd6c5fe982
matthewpblock <matthewpblock@gmail.com>
2025-02-08 09:15:10 -1000

    updated with extr columns, ordered by dependency

--- sql_create/02_create_tables.sql
+++ sql_create/02_create_tables.sql
@@ -1,37 +1,32 @@
 -- create your database schema using sql
 
--- create_tables.sql - a SQL script containing SQL statements (not Python)
--- NOTE: SQL requires semicolons at the end of each statement
--- Comments start with two dashes and a space
-
 -- Start by deleting any tables if the exist already
 -- We want to be able to re-run this script as needed.
 -- DROP tables in reverse order of creation 
 -- DROP dependent tables (with foreign keys) first
 
-DROP TABLE IF EXISTS teams;
 DROP TABLE IF EXISTS players;
+DROP TABLE IF EXISTS teams;
 
--- Create the books table
--- Note that the books table has a foreign key to the authors table
--- This means that the books table is dependent on the authors table
--- Be sure to create the standalone authors table BEFORE creating the books table.
-
+-- Create the teams table
 CREATE TABLE teams (
-    team TEXT PRIMARY KEY,
-    nickname TEXT,
-    city TEXT,
-    'state' TEXT,
+    team TEXT PRIMARY KEY,
+    nickname TEXT,
+    city TEXT,
+    'state' TEXT,
+    conference TEXT,
+    division TEXT,
 );
 
--- Create the authors table 
--- Note that the author table has no foreign keys, so it is a standalone table
-
+-- Create the playes table with a foreign key to the teams table
 CREATE TABLE players (
-    player_id TEXT PRIMARY KEY,
+    player_id TEXT PRIMARY KEY,
     first_name TEXT,
     last_name TEXT,
-    birthdate INTEGER
+    birthdate INTEGER,
+    height TEXT,
+    'weight' INTEGER,
+    country TEXT,
     FOREIGN KEY (team) REFERENCES teams(team)
 
 );
